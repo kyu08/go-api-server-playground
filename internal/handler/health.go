f4532eda859515629cfdb5a7bae2c1a74104701d
kyu08 <49891479+kyu08@users.noreply.github.com>
2024-08-12 19:34:20 +0900

    DBへの接続処理のサンプルをコメントアウト

--- internal/handler/health.go
+++ internal/handler/health.go
@@ -2,28 +2,28 @@ package handler
 
 import (
 	"context"
-	"database/sql"
 	"log"
 
 	pb "github.com/kyu08/go-api-server-playground/pkg/grpc"
-	oursql "github.com/kyu08/go-api-server-playground/sql"
 )
 
 func (s *TwitterServer) Health(ctx context.Context, _ *pb.HealthRequest) (*pb.HealthResponse, error) {
 	log.Printf("Received: %v", "Health")
 
-	testSQL(ctx, s.db)
+	// testSQL(ctx, s.db)
 
 	return &pb.HealthResponse{Message: "twitter"}, nil
 }
 
-func testSQL(ctx context.Context, db *sql.DB) {
-	queries := oursql.New(db)
-
-	authors, err := queries.ListAuthors(ctx)
-	if err != nil {
-		panic(err)
-	}
-
-	log.Printf("authors: %v\n", authors)
-}
+// TODO: あとで消す
+// こんな感じで接続できるよーというサンプル。
+// func testSQL(ctx context.Context, db *sql.DB) {
+// 	queries := oursql.New(db)
+//
+// 	authors, err := queries.ListAuthors(ctx)
+// 	if err != nil {
+// 		panic(err)
+// 	}
+//
+// 	log.Printf("authors: %v\n", authors)
+// }
