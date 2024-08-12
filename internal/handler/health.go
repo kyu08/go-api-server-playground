2dadf7ba6010372742a2e714f59dc5d2ce9d27f5
kyu08 <49891479+kyu08@users.noreply.github.com>
2024-08-13 01:06:02 +0900

    DBに接続するhandlerのE2Eテストを追加

--- internal/handler/health.go
+++ internal/handler/health.go
@@ -10,20 +10,5 @@ import (
 func (s *TwitterServer) Health(ctx context.Context, _ *pb.HealthRequest) (*pb.HealthResponse, error) {
 	log.Printf("Received: %v", "Health")
 
-	// testSQL(ctx, s.db)
-
 	return &pb.HealthResponse{Message: "twitter"}, nil
 }
-
-// TODO: あとで消す
-// こんな感じで接続できるよーというサンプル。
-// func testSQL(ctx context.Context, db *sql.DB) {
-// 	queries := oursql.New(db)
-//
-// 	authors, err := queries.ListAuthors(ctx)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-//
-// 	log.Printf("authors: %v\n", authors)
-// }
