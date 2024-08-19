e9ab1dbc429cb21a74953edb69fb42042c8dc6a6
kyu08 <49891479+kyu08@users.noreply.github.com>
2024-08-19 23:14:27 +0900

    wip

--- database/schema/schema.sql
+++ database/schema/schema.sql
@@ -1,6 +1,6 @@
 create table user (
   id varchar(36) not null primary key,
-  screen_name varchar(20) not null, -- @twitter の@以降の部分
+  screen_name varchar(20) not null unique, -- @twitter の@以降の部分
   user_name varchar(20) not null, -- `name`にしたかったがSQLのキーワードなので仕方なく`user_name`にしている
   bio varchar(160) not null,
   is_private boolean not null,
--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -10,7 +10,10 @@ import (
 	"github.com/kyu08/go-api-server-playground/internal/domain/user"
 )
 
-var ErrFindUserByScreenNameUserNotFound = errors.New("user not found")
+var (
+	ErrFindUserByScreenNameUserNotFound = errors.New("user not found")
+	ErrCreateUserScreenNameAlreadyUsed  = errors.New("the screen name specified is already used")
+)
 
 type UserRepository struct{}
 
@@ -18,14 +21,23 @@ func NewUserRepository() *UserRepository {
 	return &UserRepository{}
 }
 
-func (UserRepository) Create(ctx context.Context, db *sql.DB, u *user.User) error {
+func (r UserRepository) Create(ctx context.Context, db *sql.DB, u *user.User) error {
 	tx, err := db.BeginTx(ctx, nil)
+	if err != nil {
+		return fmt.Errorf("db.BeginTx: %w", err)
+	}
+
 	defer tx.Commit()
 	queries := database.New(tx)
 
 	// TODO: これを参考にトランザクションの処理をいい感じにかく(そしてそれをdatabaseパッケージに移動する)
-	// TODO: すでに同名のscreenNameのuserが存在しないことを確認する
 	// TODO: ↑の処理は誰がやるべきなんだ... domainService的な存在が必要かも？
+	if u, err := r.FindByScreenName(ctx, db, u.ScreenName.String()); u != nil {
+		return ErrCreateUserScreenNameAlreadyUsed
+	} else if err != nil && !errors.Is(err, ErrFindUserByScreenNameUserNotFound) {
+		return fmt.Errorf("r.FindByScreenName: %w", err)
+	}
+
 	// TODO: screenNameのユニーク制約をDBにかける
 	_, err = queries.CreateUser(ctx, database.CreateUserParams{
 		ID:         u.ID.String(),
