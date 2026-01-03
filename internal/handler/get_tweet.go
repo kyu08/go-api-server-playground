ff9b7773ead1507dce7b9f03d90c8c7d844bf602
kyu08 <49891479+kyu08@users.noreply.github.com>
2026-01-03 20:51:52 +0900

    handler

--- internal/handler/get_tweet.go
+++ internal/handler/get_tweet.go
@@ -3,7 +3,6 @@ package handler
 import (
 	"context"
 
-	"github.com/kyu08/go-api-server-playground/internal/apperrors"
 	"github.com/kyu08/go-api-server-playground/internal/usecase"
 	"github.com/kyu08/go-api-server-playground/proto/api"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -11,10 +10,9 @@ import (
 
 func (s *TwitterServer) GetTweet(ctx context.Context, req *api.GetTweetRequest) (*api.GetTweetResponse, error) {
 	input := usecase.NewTweetGetInput(req.GetTweetId())
-
 	output, err := s.TweetGetUsecase.Run(ctx, input)
 	if err != nil {
-		return nil, apperrors.WithStack(err)
+		return nil, err
 	}
 
 	return &api.GetTweetResponse{
